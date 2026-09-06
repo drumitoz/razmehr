d14f6cb82fdec24ff4f8ce3a1b97ea30f077aa10
drumitoz <omid.kheyrkhah1991@gmail.com>
2026-09-06 23:54:35 +0300

    Add isolated Beauty News feature flag

---
+++ feature-flags.js
@@ -0,0 +1,12 @@
+/*
+ * کلیدهای مستقل امکانات سایت رازمهر
+ * برای خارج‌کردن کامل بیوتی نیوز از نمایش عمومی، مقدار beautyNews را false کنید.
+ */
+window.RAZMEHR_FEATURES = Object.freeze({
+  beautyNews: true
+});
+
+document.documentElement.classList.toggle(
+  'feature-beauty-news-off',
+  !window.RAZMEHR_FEATURES.beautyNews
+);
