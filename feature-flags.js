ea656086f25b78030d01a869b128bd5e90694cd0
drumitoz <omid.kheyrkhah1991@gmail.com>
2026-09-12 22:53:21 +0300

    chore: disable birthday theme immediately

--- feature-flags.js
+++ feature-flags.js
@@ -4,8 +4,7 @@
  */
 window.RAZMEHR_FEATURES = Object.freeze({
   beautyNews: true,
-  birthdayTheme: true,
-  birthdayThemeEndsAt: '2026-09-13T11:00:00+03:30'
+  birthdayTheme: false
 });
 
 document.documentElement.classList.toggle(
